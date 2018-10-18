c784243fb2bc84a5b4848da2cbc26bcf96549ba4
Joshua Redding <jmichael652@gmail.com>
2018-10-18 14:38:39 -0400

    Cleans up default file headers and packages

---
+++ src/test/Main.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * Exists to provide simple testing for the project
+ * @author JMichael652
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
