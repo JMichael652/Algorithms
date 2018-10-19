6d3e1f698f8a6a92b4aa0490d66d188fab31fad3
Joshua Redding <jmichael652@gmail.com>
2018-10-18 20:22:17 -0400

    Provides tests for SLNode

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,5 +1,7 @@
 package test;
 
+import datastructures.lists.*;
+
 /**
  * Exists to provide simple testing for the project
  * @author JMichael652
@@ -10,7 +12,48 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        testSLNode();
+    }
+    
+    private static void testSLNode() {
+        
+        System.out.println("Test 1");
+        SLNode a = new SLNode(null);
+        Tests.testEquals(a.getData(), null);
+        Tests.testEquals(a.getNext(), null);
+        
+        System.out.println("Test 2");
+        SLNode b = new SLNode(null, null);
+        Tests.testEquals(b.getData(), null);
+        Tests.testEquals(b.getNext(), null);
+        
+        System.out.println("Test 3");
+        String c = "";
+        SLNode<String> d = new SLNode<>(c);
+        Tests.testEquals(d.getData(), c);
+        Tests.testEquals(d.getNext(), null);
+        
+        System.out.println("Test 4");
+        SLNode<String> e = new SLNode(c, null);
+        Tests.testEquals(e.getData(), c);
+        Tests.testEquals(e.getNext(), null);
+        
+        System.out.println("Test 5");
+        SLNode<String> f = new SLNode(c, d);
+        Tests.testEquals(f.getData(), c);
+        Tests.testEquals(f.getNext(), d);
+        
+        System.out.println("Test 6");
+        SLNode<String> g = new SLNode(c, d);
+        g.setData(null);
+        Tests.testEquals(g.getData(), null);
+        Tests.testEquals(g.getNext(), d);
+        
+        System.out.println("Test 7");
+        SLNode<String> h = new SLNode(c, d);
+        h.setNext(null);
+        Tests.testEquals(h.getData(), c);
+        Tests.testEquals(h.getNext(), null);
     }
     
 }
